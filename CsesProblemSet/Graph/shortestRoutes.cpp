bdaf91edbf35726db33e06e4947fa96b22661d92
Amit <amity552000@gmail.com>
2022-02-02 11:46:30 +0530

    Added

---
+++ CsesProblemSet/Graph/shortestRoutes.cpp
@@ -0,0 +1,86 @@
+#include<bits/stdc++.h>
+//#include<ext/pb_ds/assoc_container.hpp>
+//using namespace __gnu_pbds;
+using namespace std;
+#define ll 				long long int
+#define ld				long double
+#define mod             1000000007
+#define inf             1e18
+#define endl			"\n"
+#define pb 				emplace_back
+#define vi              vector<ll>
+#define vs				vector<string>
+#define pii             pair<ll,ll>
+#define ump				unordered_map
+#define mp 				map
+#define pq_max          priority_queue<ll>
+#define pq_min          priority_queue<ll,vi,greater<ll> >
+#define ff 				first
+#define ss 				second
+#define mid(l,r)        (l+(r-l)/2)
+#define loop(i,a,b) 	for(int i=(a);i<=(b);i++)
+#define looprev(i,a,b) 	for(int i=(a);i>=(b);i--)
+#define log(args...) 	{ string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); err(_it, args); }
+#define logarr(arr,a,b)	for(int z=(a);z<=(b);z++) cout<<(arr[z])<<" ";cout<<endl;	
+#define token(str,ch)	(std::istringstream var((str)); vs v; string t; while(getline((var), t, (ch))) {v.pb(t);} return v;)
+vs tokenizer(string str,char ch) {std::istringstream var((str)); vs v; string t; while(getline((var), t, (ch))) {v.pb(t);} return v;}
+
+
+void err(istream_iterator<string> it) {}
+template<typename T, typename... Args>
+void err(istream_iterator<string> it, T a, Args... args) {
+	cout << *it << " = " << a << endl;
+	err(++it, args...);
+}
+//typedef tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+
+void file_i_o()
+{
+    ios_base::sync_with_stdio(0); 
+    cin.tie(0); 
+    cout.tie(0);
+	#ifndef ONLINE_JUDGE
+	    freopen("input.txt", "r", stdin);
+	    freopen("output.txt", "w", stdout);
+	#endif
+}
+int main(int argc, char const *argv[]) {
+	file_i_o();
+	ll n,m,q;
+	std::cin>>n>>m>>q;
+	vector<vector<ll>> dp(n+1,vector<ll>(n+1));
+	loop(i,1,n){
+		loop(j,1,n){
+			if(i==j){
+				dp[i][j]=0;
+			}
+			else{
+				dp[i][j]=inf;
+			}
+		}
+	}
+	for(int i=0;i<m;i++){
+		ll u,v,wt;
+		std::cin>>u>>v>>wt;
+		dp[u][v]=std::min(dp[u][v],wt);
+		dp[v][u]=std::min(dp[v][u],wt);
+	}
+	for(int x=1;x<=n;x++){
+		for(int i=1;i<=n;i++){
+			for(int j=1;j<=n;j++){
+				dp[i][j]=std::min(dp[i][j],dp[i][x]+dp[x][j]);
+			}
+		}
+	}
+	while(q--){
+		ll a,b;
+		std::cin>>a>>b;
+		if(dp[a][b]==inf){
+			std::cout<<"-1"<<"\n";
+		}
+		else{
+			std::cout<<dp[a][b]<<"\n";
+		}
+	}
+	return 0;
+}
